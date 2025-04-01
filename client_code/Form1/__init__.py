2ff523ea7f21c6ea999158287ad967e1c672862b
love+freeplan@anvil.works <love+freeplan@anvil.works>
2025-04-01 15:46:58 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,18 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.person = app_tables.person.get(Name="Love")
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert(title=f"Hello {self.person['Name']}, You are {self.person['Age']}")
